f2f29bd7d3af46d01110b8f2f09f008c868300fd
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-12-18 07:52:36 -0500

    Create passByValue.py

---
+++ passByValue.py
@@ -0,0 +1,13 @@
+def change_number(input_num):
+    # Modify the number inside the function
+    input_num= 4
+    print("Inside the function:", input_num)
+
+# Define a number
+my_num = 1 
+
+# Call the function with the number
+change_number(my_num)
+
+# Print the original number outside the function
+print("Outside the function:", my_num)
